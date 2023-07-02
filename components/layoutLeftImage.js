229b37d249f911aaee7026bcabb2dd545ef5c331
Elena Aseeva <aseeva.e.s@gmail.com>
2023-07-02 22:24:30 +0300

    font-family-roboto added

--- components/layoutLeftImage.js
+++ components/layoutLeftImage.js
@@ -1,11 +1,21 @@
 import Image from "next/image";
 import Layout from "./layout";
 import defaultImage from "/public/images/left-box.jpg";
+import { Roboto } from "next/font/google";
 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 export default function LayoutLeftImage({ children, image }) {
+
   return (
     <Layout>
-      <div className="flex h-screen w-screen">
+      <div className={roboto.className}>
+
+      
+      <div className=" flex h-screen w-screen">
         <div className="relative flex w-1/2">
           <Image
             src={image || defaultImage}
@@ -18,6 +28,7 @@ export default function LayoutLeftImage({ children, image }) {
         <div className="relative flex items-center justify-center w-1/2 p-4 overflow-y-scroll">
           {children}
         </div>
+      </div>
       </div>
     </Layout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const mainFont =  Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Home() {
   return (
     <main
-      className={`main flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`main flex min-h-screen flex-col items-center justify-between p-24 ${mainFont.className}`}
     >
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
